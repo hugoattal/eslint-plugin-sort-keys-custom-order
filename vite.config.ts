5b217d39a77bc52efacb061f19c33a9310be41b7
Hugo <hugoattal@hotmail.fr>
2022-08-08 13:54:58 +0200

    Replace Jest by Vitest

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+import * as path from "path";
+import { defineConfig, UserConfig } from "vite";
+
+const alias = {
+    "@": path.resolve(__dirname, "src")
+};
+
+export default defineConfig(() => {
+    const config: UserConfig = {
+        resolve: {
+            alias
+        }
+    };
+
+    config.test = {
+        globals: true
+    };
+
+    return config;
+});
