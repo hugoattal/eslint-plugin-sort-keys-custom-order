c6f05a9ed18feda9b24eac942c913fcc7027cde2
Hugo <hugoattal@hotmail.fr>
2022-11-28 00:45:08 +0100

    Forgot to export the new rules...

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import objectKeysRule from "./rules/objectKeys";
 import typeKeysRule from "./rules/typeKeys";
+import importObjectKeysRule from "./rules/importObject";
+import exportObjectKeysRule from "./rules/exportObject";
 
 export const rules = {
+    "export-object-keys": exportObjectKeysRule,
+    "import-object-keys": importObjectKeysRule,
     "object-keys": objectKeysRule,
     "type-keys": typeKeysRule
 };
