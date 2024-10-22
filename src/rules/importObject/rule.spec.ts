bc389033d79073f8833c928837e6547d6570ecf3
Hugo ATTAL <4563971+hugoattal@users.noreply.github.com>
2024-10-22 10:00:49 +0200

    Update packages version

--- src/rules/importObject/rule.spec.ts
+++ src/rules/importObject/rule.spec.ts
@@ -1,13 +1,9 @@
-import { RuleTester, RuleTesterConfig } from "@typescript-eslint/rule-tester";
+import { RuleTester } from "@typescript-eslint/rule-tester";
 import { invalid, valid } from "./ruleTester";
 import { rule } from "./index";
 
-const ruleTesterConfig: RuleTesterConfig = {
-    parser: "@typescript-eslint/parser"
-};
-
 describe("import-object-keys", () => {
-    const ruleTester = new RuleTester(ruleTesterConfig);
+    const ruleTester = new RuleTester();
 
     ruleTester.run(
         "import-object-keys",
